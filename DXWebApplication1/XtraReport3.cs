5a80c41d38983d737cefdee95dd16bcf2fa00c16
Grupo03 - Luis Piovan <luisguilherme.piovan@hotmail.com>
2021-09-08 13:14:01 -0300

    Included ReportDesign with database blank type

---
+++ DXWebApplication1/XtraReport3.cs
@@ -0,0 +1,13 @@
+using System;
+using DevExpress.XtraReports.UI;
+
+namespace DXWebApplication
+{
+    public partial class XtraReport3
+    {
+        public XtraReport3()
+        {
+            InitializeComponent();
+        }
+    }
+}
